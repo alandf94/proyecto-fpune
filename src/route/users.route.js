0acbc2369f49da5c6f01dfd5374b05fba3ebb322
Alan Duarte <adu@cse.edu.py>
2023-05-19 20:23:44 -0400

    EXAMEN LPV 2nda parcial

--- src/route/users.route.js
+++
@@ -1,8 +0,0 @@
-const userController = require('../controller/users.controller');
-
-module.exports = function (app) {
-    app.get("/users/list", userController.listar);
-    app.get("/user/:id", userController.consultarPorCodigo);
-    app.post("/users/update", userController.actualizar);
-    app.delete("/users/delete/:id", userController.eliminar);
-}
